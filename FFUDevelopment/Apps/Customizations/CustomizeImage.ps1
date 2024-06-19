287ff47a3ec9518e0163c3de7a412edd1103dc22
Zehadi Alam <63765084+zehadialam@users.noreply.github.com>
2024-06-19 13:12:53 -0400

    Modify CustomizeImage.ps1 to fix registry settings modifications

--- FFUDevelopment/Apps/Customizations/CustomizeImage.ps1
+++ FFUDevelopment/Apps/Customizations/CustomizeImage.ps1
@@ -41,32 +41,47 @@ try {
 
 Import-StartLayout -LayoutPath "D:\Customizations\taskbar.xml" -MountPath "C:\"
 
-Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Session Manager\Configuration Manager" -Name "EnablePeriodicBackup" -Value 1 -Type "REG_DWORD"
+function Set-RegistrySetting {
+    param (
+        $RegPath,
+        $RegValueName,
+        $RegValueType,
+        $RegValueData
+    )
+    if (-not (Test-Path -Path $RegPath)) {
+        New-Item -Path Registry::$RegPath.Replace(":", "")
+    }
+    New-ItemProperty -Path $RegPath -Name $RegValueName -PropertyType $RegValueType -Value $RegValueData
+}
+
+# Enable periodic registry backups
+Set-RegistrySetting -RegPath "HKLM:\System\CurrentControlSet\Control\Session Manager\Configuration Manager" -RegValueName "EnablePeriodicBackup" -RegValueType DWORD -RegValueData 1
 # Configures the Chat icon on the taskbar
-Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\Windows Chat" -Name "ChatIcon" -Value 3 -Type "REG_DWORD"
+Set-RegistrySetting -RegPath "HKLM:\Software\Policies\Microsoft\Windows\Windows Chat" -RegValueName "ChatIcon" -RegValueType DWORD -RegValueData 3
 # Disable widgets
-Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Dsh" -Name "AllowNewsAndInterests" -Value 0 -Type "REG_DWORD"
+Set-RegistrySetting -RegPath "HKLM:\Software\Policies\Microsoft\Dsh" -RegValueName "AllowNewsAndInterests" -RegValueType DWORD -RegValueData 0
 # Do not show Windows tips
-Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\CloudContent" -Name "DisableSoftLanding" -Value 1 -Type "REG_DWORD"
+Set-RegistrySetting -RegPath "HKLM:\Software\Policies\Microsoft\Windows\CloudContent" -RegValueName "DisableSoftLanding" -RegValueType DWORD -RegValueData 1
 # Turn off cloud consumer account state content
-Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\CloudContent" -Name "DisableConsumerAccountStateContent" -Value 1 -Type "REG_DWORD"
+Set-RegistrySetting -RegPath "HKLM:\Software\Policies\Microsoft\Windows\CloudContent" -RegValueName "DisableConsumerAccountStateContent" -RegValueType DWORD -RegValueData 1
 # Turn off Microsoft consumer experiences
-Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\CloudContent" -Name "DisableWindowsConsumerFeatures" -Value 1 -Type "REG_DWORD"
+Set-RegistrySetting -RegPath "HKLM:\Software\Policies\Microsoft\Windows\CloudContent" -RegValueName "DisableWindowsConsumerFeatures" -RegValueType DWORD -RegValueData 1
+# Copy lock screen background
 Copy-Item -Path "D:\Customizations\lockscreen.jpg" -Destination "C:\Windows\Web\Screen" -Force
 # Set lock screen
-Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\Personalization" -Name "LockScreenImage" -Value "C:\Windows\Web\Screen\lockscreen.jpg" -Type "REG_SZ"
-# Prevent changing lock screen and logon image
-Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\Personalization" -Name "NoChangingLockScreen" -Value 0 -Type "REG_DWORD"
+Set-RegistrySetting -RegPath "HKLM:\Software\Policies\Microsoft\Windows\Personalization" -RegValueName "LockScreenImage" -RegValueType String -RegValueData "C:\Windows\Web\Screen\lockscreen.jpg"
+# Allow changing lock screen and logon image
+Set-RegistrySetting -RegPath "HKLM:\Software\Policies\Microsoft\Windows\Personalization" -RegValueName "NoChangingLockScreen" -RegValueType DWORD -RegValueData 0
 # Turn off fun facts, tips, tricks, and more on lock screen
-Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\Personalization" -Name "LockScreenOverlaysDisabled" -Value 1 -Type "REG_DWORD"
+Set-RegistrySetting -RegPath "HKLM:\Software\Policies\Microsoft\Windows\Personalization" -RegValueName "LockScreenOverlaysDisabled" -RegValueType DWORD -RegValueData 1
+# Use Windows Hello for Business
+Set-RegistrySetting -RegPath "HKLM:\Software\Policies\Microsoft\PassportForWork" -RegValueName "Enabled" -RegValueType DWORD -RegValueData 1
+# Do not start Windows Hello provisioning after sign-in
+Set-RegistrySetting -RegPath "HKLM:\Software\Policies\Microsoft\PassportForWork" -RegValueName "DisablePostLogonProvisioning" -RegValueType DWORD -RegValueData 1
 
+# Copy theme file to "C:\Users\Default\AppData\Local\Microsoft\Windows\Themes"
 $themesFolder = "C:\Users\Default\AppData\Local\Microsoft\Windows\Themes"
 if (-not (Test-Path $themesFolder -PathType Container)) {
     New-Item -Path $themesFolder -ItemType Directory
     Copy-Item -Path "D:\Customizations\oem.theme" -Destination $themesFolder -Force
 }
-
-# Use Windows Hello for Business
-Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\PassportForWork" -Name "Enabled" -Value 1 -Type "REG_DWORD"
-# Do not start Windows Hello provisioning after sign-in
-Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\PassportForWork" -Name "DisablePostLogonProvisioning" -Value 1 -Type "REG_DWORD"
